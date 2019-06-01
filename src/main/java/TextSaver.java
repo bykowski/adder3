b7331e68bc3e45a673607eb40d74b7b9b18e9a50
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-06-01 11:18:43 +0200

    save and read file

---
+++ src/main/java/TextSaver.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TextSaver {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bufferedWriter =
+                new BufferedWriter(new FileWriter("text.txt"));
+        String name = "Przemek";
+        bufferedWriter.write(name);
+        bufferedWriter.close();
+
+    }
+}

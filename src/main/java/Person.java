806701200f18f82df1e6011b4bde581490fa9073
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-06-01 10:27:31 +0200

    verion of person

---
+++ src/main/java/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+
+    private String name;
+    private Sex sex;
+
+    public Person(String name, Sex sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Person{" +
+//                "name='" + name + '\'' +
+//                ", sex=" + sex +
+//                '}';
+//    }
+}
--- src/main/java/SexChecker.java
+++ src/main/java/SexChecker.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class SexChecker {
 
-
     public static void main(String[] args) {
-        List<String> nameList = new ArrayList<>();
+        List<Person> nameList = new ArrayList<>();
+        Person p1 = new Person("Karolina", Sex.FEMALE);
+        Person p2 = new Person("Tomasz", Sex.MALE);
+        nameList.add(p1);
+        nameList.add(p2);
+        nameList.add(p2);
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Podaj imie:");
@@ -15,11 +20,12 @@ public class SexChecker {
             if (name.equals("koniec"))
                 break;
 
-            nameList.add(name);
             if (name.endsWith("a")) {
                 System.out.println("To jest imie zenskie");
+                nameList.add(new Person(name, Sex.FEMALE));
             } else {
                 System.out.println("To jest imie meskie");
+                nameList.add(new Person(name, Sex.MALE));
             }
         }
         System.out.println(nameList);
